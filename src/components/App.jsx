fc0e5c34392b85f0ab80302b490adddd6f72ee03
kinjoyuji <kinjoyuji@gmail.com>
2017-10-09 22:01:02 +0900

    onChangeNameイベントの追記

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Jone',
+    };
+  }
+
+  handleNameChange(name) {
+    this.setState({ name });
+  }
+
   render() {
     return (
-      <h1>HelloWorld!!!!!</h1>
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handleNameChange(e.target.value)}
+        />
+        <p>{this.state.name}</p>
+      </div>
     );
   }
 }
