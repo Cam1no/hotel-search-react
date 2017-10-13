d8c5ad0265a6df4cdad18fa754ff099bfab4ac0c
kinjoyuji <kinjoyuji@gmail.com>
2017-10-14 01:27:43 +0900

    Ajax通信部分を切り出し、リファクタ

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
-import Map from './Map'
-
-const GEOCODE_ENDPOINT = 'http://maps.googleapis.com/maps/api/geocode/json'
+import Map from './Map';
+import { geocoder } from '../domain/Geocoder';
 
 class App extends Component {
   constructor(props) {
@@ -29,18 +27,11 @@ class App extends Component {
   }
 
   handlePlaceSubmit(place) {
-    axios
-      .get(GEOCODE_ENDPOINT, { params: { address: place } })
-      .then(results => {
-        console.log(results);
-        const data = results.data
-        const result = data.results[0];
-        switch (data.status) {
+    geocoder(place)
+      .then(({ status, address, location }) => {
+        switch (status) {
           case 'OK': {
-            this.setState({
-              address: result.formatted_address,
-              location: result.geometry.location,
-            });
+            this.setState({address, location});
             break;
           }
           case 'ZERO_RESULTS': {
