8742be26620d4ae616d2fe8dba5ad09ec7de3e5c
kinjoyuji <kinjoyuji@gmail.com>
2017-10-14 00:15:11 +0900

    Mapのサイズを修正

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 
 const InnterMap = withGoogleMap(props =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={15}
+    defaultCenter={props.position}
+    center={props.position}
   >
+  <Marker { ...props.marker } />
   </GoogleMap>
 );
 
-const Map = props => (
-  <InnterMap
-    containerElement={(<div />)}
-    mapElement={(<div className="map"/>)}
-  />
-)
+const Map = ({ lat, lng }) => {
+  const position = { lat, lng };
+  return (
+    <InnterMap
+      containerElement={(<div />)}
+      mapElement={(<div className="map" />)}
+      position={position}
+      marker={{ position }}
+    />
+  );
+};
 
+Map.propTypes = {
+  lat: PropTypes.number,
+  lng: PropTypes.number,
+};
+
+Map.defaultProps = {
+  lat: -34.397,
+  lng: 150.644,
+};
 
 export default Map;
